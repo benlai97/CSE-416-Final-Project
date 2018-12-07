c942ea446199a0a027f00ed7f75af6829795d158
Jonathan Chen <jonathan.chen@wustl.edu>
2018-12-06 23:41:04 -0600

    fixed excess degree math

--- lib/analysis/dist.py
+++ lib/analysis/dist.py
@@ -14,18 +14,20 @@ def excess_degree(g: gt.Graph, deg='total') -> np.array:
     '''
     Returns array of excess degree frequencies.
     '''
+
     def compute_excess(k, degrees):
         edges = g.get_edges()[:, :2]
         # find all nodes with degree k+1
-        nodes = g.vp['id'].a[degrees == k + 1]
+        nodes = g.get_vertices()[degrees == k + 1]
         # find all edges that contain those nodes
-        associated_edges = np.isin(edges, nodes)
+        associated_edges = np.any(np.isin(edges, nodes), axis=1)
         # return the number of edges found
         return np.sum(associated_edges)
 
     degrees = g.degree_property_map(deg=deg).a
+    q = np.vectorize(lambda k: compute_excess(k, degrees))
     k = np.arange(1, degrees.max())
-    counts = compute_excess(k,degrees)
+    counts = q(k)
     return counts / np.sum(counts)
 
 def power(g: gt.Graph, a_hat, k_min=1):
