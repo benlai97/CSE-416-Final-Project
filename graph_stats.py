9fe314c85acece348ccf1e1f6abfeefd0d6d23bb
Catherine Jiang <catejiang@wustl.edu>
2018-12-07 03:06:43 -0600

    add summary and graph stats function

---
+++ graph_stats.py
@@ -0,0 +1,51 @@
+import graph_tool.all as gt
+import numpy as np
+from lib.analysis import clustering
+from lib.analysis import dist
+from lib.analysis import model
+from lib.analysis import power
+
+    # Input: gt.Graph
+    # Returns comma-separated values:
+    #     Number of edges
+    #     Number of vertices
+    #     Average clustering coefficient
+    #     Average degree
+    #     Average excess degree
+    #     Average path length
+    #     Size of largest connected component
+    #     Number of connected components
+def stats(g: gt.Graph):
+    
+    # Number of edges
+    num_edges = g.num_edges()
+    
+    # Number of vertices
+    num_vertices = g.num_vertices()
+    
+    # Clustering coefficient
+    clustering_coefficient = clustering.coefficient(g)
+    
+    # Average degree
+    avg_degree = np.mean(dist.degree(g))
+    
+    # Average excess degree
+    excess_degree = np.mean(dist.excess_degree(g))
+    
+    # Average path length
+    all_sd = gt.shortest_distance(g)
+    avg_path = np.mean(list(all_sd))
+    
+    # Size of largest connected component
+    l = gt.label_largest_component(g)
+    u = gt.GraphView(g, vfilt=l)
+    lcc = u.num_vertices()
+    
+    # Number of connected components
+    labeled = gt.label_components(g)
+    num_cc = max(np.unique(labeled[0].a))
+    
+    return num_edges, num_vertices, clustering_coefficient, avg_degree, excess_degree, avg_path, lcc, num_cc
+
+if __name__ == "__main__":
+    pass
