8dd837ac1f8e63bf860d8361102331958fce752e
Jonathan Chen <jonathan.chen@wustl.edu>
2018-12-09 14:44:03 -0600

    added docstring for betweeness

--- lib/analysis/centrality.py
+++ lib/analysis/centrality.py
@@ -3,7 +3,8 @@ import graph_tool.all as gt
 
 def betweeness(g: gt.Graph) -> (gt.PropertyMap, gt.PropertyMap):
     '''returns both vertex and edge betweeness'''
-    return gt.betweeness(g, norm=True)
+    vertex, edge = gt.betweeness(g, norm=True)
+    return vertex, edge
 
 
 if __name__ == '__main__':
