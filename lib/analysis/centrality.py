40d2e9833ea375f91c298a73f87e41fdc4515fb4
Benjamin Lai <benlai@wustl.edu>
2018-12-09 16:03:22 -0600

    centrality measures

--- lib/analysis/centrality.py
+++ lib/analysis/centrality.py
@@ -1,11 +1,20 @@
 import graph_tool.all as gt
 
-
-def betweenness(g: gt.Graph) -> (gt.PropertyMap, gt.PropertyMap):
-    '''returns both vertex and edge betweenness'''
-    vertex, edge = gt.betweenness(g, norm=True)
+def betweeness(g: gt.Graph) -> (gt.PropertyMap, gt.PropertyMap):
+    '''returns both vertex and edge betweeness'''
+    vertex, edge = gt.betweeness(g, norm=True)
     return vertex, edge
 
+def closeness(g: gt.Graph) -> gt.PropertyMap:
+    '''returns vertex closeness'''
+    vertex = gt.closeness(g, norm=True)
+    return vertex
+
+def eigenvector(g: gt.Graph) -> (np.float6, gt.Propertymap):
+    '''returns eigenvector and largest eigvenvalue'''
+    value, vector = gt.eigenvector(g)
+    return value, vector
 
 if __name__ == '__main__':
     pass
+
